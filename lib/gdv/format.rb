83b2da60cdd4b6299bbf5ae1d0bbc4685b23400c
David Lutterkort <lutter@vendis.org>
2010-07-25 15:05:23 -0700

    * format/parser.rb: move parser into separate file

--- lib/gdv/format.rb
+++ lib/gdv/format.rb
@@ -2,6 +2,7 @@ require 'gdv/format/line'
 require 'gdv/format/rectype'
 require 'gdv/format/recindex'
 require 'gdv/format/reader'
+require 'gdv/format/parser'
 
 module GDV::Format
     def self.load_rectypes(file)
--- lib/gdv/format/reader.rb
+++ lib/gdv/format/reader.rb
@@ -71,64 +71,6 @@ module GDV::Format
     class Reader
         attr_reader :io, :lineno, :unknown
 
-        # Helper class for the DSL used in Reader.parse
-        class Parser
-            attr_reader :result
-
-            def initialize(reader, klass, &block)
-                @reader = reader
-                @result = klass.new
-                instance_eval &block
-            end
-
-            def one(sym, cond)
-                result[sym] = @reader.match!(cond)
-            end
-
-            def maybe(sym, cond)
-                result[sym] = @reader.match(cond)
-            end
-
-            def star(sym, cond)
-                result[sym] = []
-                while @reader.match?(cond)
-                    result[sym] << @reader.getrec
-                end
-            end
-
-            def object(sym, klass)
-                result[sym] = klass.parse(@reader)
-            end
-
-            # Parse a sequence of objects of class +klass+ as long
-            # as +cond+ matches the current record
-            def objects(sym, klass, cond)
-                result[sym] = []
-                while @reader.match?(cond)
-                    result[sym] << klass.parse(@reader)
-                end
-            end
-
-            # Skip records until we find one that matches +cond+
-            def skip_until(cond)
-                while ! @reader.match?(cond)
-                    @reader.getrec
-                end
-            end
-
-            def match?(cond)
-                @reader.match?(cond)
-            end
-
-            def satz?(satz)
-                match?(:satz => satz)
-            end
-
-            def sparte?(sparte)
-                match?(:sparte => sparte)
-            end
-        end
-
         def initialize(io)
             @features = [:pad_short_lines]
             if io.is_a?(String)
