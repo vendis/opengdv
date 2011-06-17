c5c9860c338bacfb065449c420124276800721fb
David Lutterkort <lutter@redhat.com>
2011-06-17 16:06:34 +0200

    * lib/gdv/model/package.rb (vmnr): get from vorsatz or nachsatz

--- lib/gdv/model/package.rb
+++ lib/gdv/model/package.rb
@@ -12,6 +12,15 @@ module GDV::Model
         property :vunr, :vorsatz, 1, 2
         property :absender, :vorsatz, 1, 3
         property :created_from_until, :vorsatz, 1, 5
+        property :vmnr, :vorsatz, 1, 6
+
+        def vmnr
+            vmnr = vorsatz[1][6]
+            if vmnr.nil? || vmnr.empty?
+                vmnr = nachsatz[1][3]
+            end
+            vmnr
+        end
 
         def created_from
             GDV::Format.parse_date(created_from_until_raw[0, 8])
